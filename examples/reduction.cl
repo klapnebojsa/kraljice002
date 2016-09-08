8ba5504cca0ce9b3039fb715deddc0dafbf511ce
klapnebojsa <klapnebojsa@gmail.com>
2016-09-08 20:29:17 +0200

    .

--- examples/reduction.cl
+++ examples/reduction.cl
@@ -1,21 +1,23 @@
 
 int okreni_90 (int broj, int polja){
-    int resenje=0;int x[6];int y[6];
+    int resenje=0;int x[8];int y[8];
     
     for (int j=polja; j > 0; j--){x[j] = broj%10; broj /= 10;}   //rasclanjivanje broja na elemente
     for (int j=1; j<=polja; j++){y[polja-x[j]+1]=j;}             //preracun za okretanje za 90 stepeni
 
-    for (int g=1; g<=polja; g++){resenje += y[g]*pow(10, g-1);} 
+    for (int g=1; g<=polja; g++){resenje += y[g]*pow(10, g-1);}
+    //printf("resenje=%u\n", resenje);  
     return resenje;       
 }
 int okreni180 (int broj, int polja){
-    int resenje=0;int x[6];int y[6];
+    int resenje=0;int x[8];int y[8];
     
     for (int j=polja; j > 0; j--){x[j] = broj%10; broj /= 10;}   //rasclanjivanje broja na elemente
-    for(int j=1; j<=polja; j++){y[polja-x[j]+1]=j;}              //preracun za okretanje za 180 stepeni
+    //for(int j=1; j<=polja; j++){y[polja-x[j]+1]=j;}              //preracun za okretanje za 180 stepeni
+    for(int j=1; j<=polja; j++){y[j]=x[j];}
     for(int j=1; j<=polja; j++){x[j]=y[polja-j+1];}
     
-    for (int g=1; g<=polja; g++){resenje += x[g]*pow(10, g-1);} 
+    for (int g=1; g<=polja; g++){resenje += x[g]*pow(10, g-1);}
     return resenje;       
 }
 
@@ -26,7 +28,7 @@ __kernel void reduction_scalar(__global int* data,
                               __local int4* partial_sums,         //resenja
                               __local int* partial_podaci,        //lokalni podaci preneti iz globalne memorije
                               __local int* x,
-                              __local int* y,                                                            
+                             // __local int* y,                                                            
                               __global int4* output) {   
    
    int tr,j,g,f;        
@@ -39,7 +41,7 @@ __kernel void reduction_scalar(__global int* data,
    barrier(CLK_LOCAL_MEM_FENCE); 
 
                 
-   for (int k=0; k<=gsize/2; k++){        
+   for (int k=0; k<=gsize; k++){        
         //j=polja; while (tr > 0){x[j] = tr%10; tr /= 10; j--;}      
         tr=partial_podaci[k]; for (int j=polja; j > 0; j--){x[j] = tr%10; tr /= 10;}    //rasclanjivanje broja na elemente
          
@@ -59,12 +61,57 @@ __kernel void reduction_scalar(__global int* data,
         i++;}
         barrier(CLK_LOCAL_MEM_FENCE);     
    } 
-     
+   //if(lid == 0) {output[get_group_id(0)] = partial_sums[lid];}
    if(lid == 0) {output[get_group_id(0)].s0 = partial_sums[lid].s0;
                  output[get_group_id(0)].s1 = partial_sums[lid].s1;
                  output[get_group_id(0)].s2 = partial_sums[lid].s2;
-                 output[get_group_id(0)].s3 = partial_sums[lid].s3;}       //Vraca vrednost pojedinacne work-group (Na kraju krajeva za svih 4,096 komada)    
+                 output[get_group_id(0)].s3 = partial_sums[lid].s3;}      
+}
+__kernel void reduction_scalar3(__global int* data,
+                              __global int* brpolja,
+                              __local int4* partial_sums,         //resenja
+                              __local int* partial_podaci,        //lokalni podaci preneti iz globalne memorije
+                              __local int* x,
+                              __local int* y,                                                            
+                              __global int4* output) {   
+   
+   int tr,j,g,f;        
+   int gid = get_global_id(0);
+   int lid = get_local_id(0);
+   int gsize = get_local_size(0);
+       
+   partial_podaci[lid] = data[gid];
+   int polja=brpolja[0];
+   //printf("gid=%u lid=%u get_group_id(0)=%u partial_podaci[lid]=%u data[gid]=%u\n",  gid, lid, get_group_id(0), partial_podaci[lid], data[gid]);
+   barrier(CLK_LOCAL_MEM_FENCE); 
 
+                
+   for (int k=0; k<=gsize; k++){        
+        //j=polja; while (tr > 0){x[j] = tr%10; tr /= 10; j--;}      
+        tr=partial_podaci[k]; for (int j=polja; j > 0; j--){x[j] = tr%10; tr /= 10;}    //rasclanjivanje broja na elemente
+         
+        int bre=1;int i=0;
+        while (i < polja && bre==1){
+            for(j=i+1; j<=polja; j++){
+                if (!((x[i]==x[j]) || (abs(x[i]-x[j])==abs(i-j)))){
+                    if (j == polja && i == polja-1) {                    //resenje
+                       partial_sums[lid].s0 = partial_podaci[k];                       //tabla osnovna sa resenjem bez okretanja                                          
+                       partial_sums[lid].s1 = okreni_90(partial_sums[lid].s0, polja);  //okrecemo tablu za 90 stepeni.  0->90
+                       partial_sums[lid].s2 = okreni180(partial_sums[lid].s0, polja);  //okrecemo tablu za 180 stepeni. 0->180
+                       partial_sums[lid].s3 = okreni180(partial_sums[lid].s1, polja);  //okrecemo tablu za 180 stepeni. 90->270                                                                                                                               
+                    }                  
+                }
+                else {bre=0;}
+            }
+        i++;}
+        barrier(CLK_LOCAL_MEM_FENCE);     
+   } 
+   //if(lid == 0) {output[get_group_id(0)] = partial_sums[lid];}
+   if(lid == 0) {output[get_group_id(0)].s0 = partial_sums[lid].s0;
+                 output[get_group_id(0)].s1 = partial_sums[lid].s1;
+                 output[get_group_id(0)].s2 = partial_sums[lid].s2;
+                 output[get_group_id(0)].s3 = partial_sums[lid].s3;                                  
+   }     
 }
  
 __kernel void reduction_scalar2(__global int* data,
@@ -78,22 +125,55 @@ __kernel void reduction_scalar2(__global int* data,
    int gid = get_global_id(0);
    int lid = get_local_id(0);
    int gsize = get_local_size(0);
-       
-   partial_podaci[lid] = data[gid];
+   
+   partial_podaci[lid] = data[gid];      
    int polja=brpolja[0];
-   barrier(CLK_LOCAL_MEM_FENCE); 
-
-                
+   barrier(CLK_LOCAL_MEM_FENCE);
+    
+   //printf("gid=%u lid=%u get_group_id(0)=%u partial_podaci[lid]=%u data[gid]=%u\n",  gid, lid, get_group_id(0), partial_podaci[lid], data[gid]); 
+   
+   int koji=0;int s=0;             
    for (int k=0; k<gsize; k++){
         trenutno=partial_podaci[k];   
         j=polja; while (trenutno > 0){x[j] = trenutno%10; trenutno /= 10; j--;}      //rasclanjivanje broja na elemente
-    
-        int bre=1;int i=0;
+                        //printf("lid=%u get_group_id(0)=%u k==%u partial_podaci[k]=%u\n",  lid, get_group_id(0), k, partial_podaci[lid]);    
+        int bre=1;int i=0;  
+        //printf("partial_podaci[k]=%u\n",  partial_podaci[k]);
         while (i < polja && bre==1){
             for(j=i+1; j<=polja; j++){
                 if (!((x[i]==x[j]) || (abs(x[i]-x[j])==abs(i-j)))){
                     if (j == polja && i == polja-1) {
-                        partial_sums[lid] = partial_podaci[k];                 
+                        partial_sums[lid] = partial_podaci[k];
+                        //if (s==0){output[get_group_id(0)].s0 = partial_podaci[k]; s=1;}
+                        //else {output[get_group_id(0)].s1 = partial_podaci[k];s=0;}
+     //switch(s)
+     //{
+     //    case 0:
+     //      output[get_group_id(0)].s0 = partial_podaci[k]; s=1;
+     //      break;
+     //    case 1:
+     //      output[get_group_id(0)].s1 = partial_podaci[k]; s=2;
+     //      break;
+     //    case 2:
+     //      output[get_group_id(0)].s2 = partial_podaci[k]; s=3;
+     //      break;
+     //    case 3:
+     //      output[get_group_id(0)].s3 = partial_podaci[k]; s=4;
+     //      break;
+     //    case 4:
+     //      output[get_group_id(0)].s4 = partial_podaci[k]; s=5;
+     //      break;
+     //    case 5:
+     //      output[get_group_id(0)].s5 = partial_podaci[k]; s=6;
+     //      break; 
+     //    case 6:
+     //      output[get_group_id(0)].s6 = partial_podaci[k]; s=7;
+     //      break;                                             
+     //    case 7:
+     //      output[get_group_id(0)].s7 = partial_podaci[k]; s=0;
+     //      break;                   
+    //}                                                                        
+                        //printf("gid=%u lid=%u get_group_id(0)=%u k==%u partial_podaci[k]=%u\n",  gid, lid, get_group_id(0), k, partial_podaci[k]);                                                                                                  
                     }
                 }
                 else {bre=0;}
@@ -102,6 +182,6 @@ __kernel void reduction_scalar2(__global int* data,
         barrier(CLK_LOCAL_MEM_FENCE);     
    } 
     
-   if(lid == 0) {output[get_group_id(0)] = partial_sums[lid];}        //Vraca vrednost pojedinacne work-group (Na kraju krajeva za svih 4,096 komada)
-    
+   if(lid == 0) {output[get_group_id(0)] = partial_sums[lid];}        //Vraca vrednost pojedinacne work-group (Na kraju krajeva za svih 1,024 komada)   
+   //  if(lid == 0) {output[get_group_id(0)] = partial_podaci[lid];}
 }
--- src/kraljice002/core.clj
+++ src/kraljice002/core.clj
@@ -35,29 +35,34 @@
    "Listing on page 225."
    (let [program-source
          (slurp (io/reader "examples/reduction.cl"))
-         num-items (Math/pow 2 15)                    ;2 na 20-tu = 1048576
-         bytesize (* num-items Float/BYTES)           ;Float/BYTES = 4    =>   bytesize = 4 * 2na20 = 4 * 1048576 = 4194304.0
+         num-items (Math/pow 2 16)                    ;2 na 16-tu = 65.536
+         bytesize (* num-items Float/BYTES)           ;Float/BYTES = 4    =>   bytesize = 4 * 2na16 = 4 * 65.536 = 262.144
          workgroup-size 256
          notifications (chan)
          follow (register notifications)
-         brpolja (int-array 1 6)         
-         data #_(int-array                  ;deo za formiranje ulaznih parametara za problem 8 dama
-                  (with-local-vars [p ()]
-                    (let [k (atom 0) n (aget brpolja 0) x (make-array Integer/TYPE n)]
-                      (while (or (pos? @k) (zero? @k))
-                        (aset x @k (inc (aget x @k)))
-                        (if-not (> (aget x @k) n)
-                          (do                            ;then 1
-                            (if (= @k (- n 1))
-                              (var-set p (conj @p (Integer. (clojure.string/join "" (vec x))))) ;then 2
-                              (aset x (swap! k inc) 0)))                                        ;else 2
-                          (swap! k dec))))               ;else 1
-                    @p))
+         brpolja (int-array 1 8)         
+         data #_(int-array   ;deo za formiranje ulaznih parametara za problem 8 dama
+                 (with-local-vars [p ()]
+                   (let [k (atom 0) n (aget brpolja 0) x (make-array Integer/TYPE n) isti (atom 0)]
+                     (while (>= @k 0)
+                       (aset x @k (inc (aget x @k)))  ;x[k]=x[k]+1
+                       (if (<= (aget x @k) n)         ;1  if x[k]<n
+                         (do                              ;then 1
+                          (if (= @k (- n 1))                ;2   if k=(n-1)
+                            (do 
+                              (reset! isti 0)
+                              (loop [q 0 w 1] (when (< q n)(if (< w n)
+                                                             (do (when (= (aget x q) (aget x w)) (reset! isti 1)) (recur q (inc w)))
+                                                             (recur (inc q) (inc (inc q))))))
+                              (when (zero? @isti) (var-set p (conj @p (Integer. (clojure.string/join "" (vec x)))))))   ;then 2 - upisi a u ulazni parametar
+                              (aset x (swap! k inc) 0)))                                                                ;else 2   x[k+1]=0
+                         (swap! k dec))))                    ;else 1     k=k-1
+                   @p))
          (int-array (clojure.edn/read-string (slurp "podaci/kraljice.dat")))     ;deo za citanje ulaznih podataka za problem 8 dama
          
          cl-partial-sums (* workgroup-size Float/BYTES)       ;4 * 256 = 1024
          ;partial-output (int-array (/ bytesize workgroup-size))      ;4*2na20 / 256 = 4*2na20 / 2na8 = 4*2na12   - niz od 16384 elemenata
-         partial-output (int-array (/ bytesize workgroup-size))        
+         partial-output (int-array (/ bytesize workgroup-size) 4)       ;4*2na16 / 256 = 2na18 / 2na8 = 2na10   - niz od 1.024 elemenata
          output (int-array 1)               ;pocetna vrednost jedan clan sa vrednoscu 0.0
          ]   
      (with-release [cl-data (cl-buffer ctx bytesize :read-only)
@@ -71,65 +76,108 @@
                     prog (build-program! (program-with-source ctx [program-source]))   ;kreira program u kontekstu ctx sa kodom programa u kojem se nalaze tri kernela 
                     ;naive-reduction (kernel prog "naive_reduction")            ;definise kernel iz prog
                     reduction-scalar (kernel prog "reduction_scalar")          ;definise kernel iz prog
-                    reduction-scalar2 (kernel prog "reduction_scalar2")          ;definise kernel iz prog                    
+                    reduction-scalar2 (kernel prog "reduction_scalar2")          ;definise kernel iz prog 
+                    reduction-scalar3 (kernel prog "reduction_scalar3")          ;definise kernel iz prog                     
                     ;reduction-vector (kernel prog "reduction_vector")          ;definise kernel iz prog
                     ;reduction-complete (kernel prog "reduction_complete")      ;definise kernel iz prog                    
                     profile-event (event)                  ;kreira novi cl_event (dogadjaj)
                     ;profile-event1 (event)                 ;          -||-
                     profile-event2 (event)                 ;          -||- 
-                    ;profile-event3 (event)                 ;          -||-       
+                    profile-event3 (event)                 ;          -||-       
                     ]
        ;(println "(apply + (float-array (range 0" num-items "))): " (apply + data))
-
+       ;(println (seq data))
        (facts
         (println "============ Napredni algoritam ======================================")
         ;; ============= Napredni algoritam ====================================
-        (set-args! reduction-scalar cl-data cl-brpolja cl-partial-sums cl-partial-podaci cl-x cl-y cl-output) => reduction-scalar
+             (set-args! reduction-scalar cl-data cl-brpolja cl-partial-sums cl-partial-podaci cl-x cl-output) => reduction-scalar
         ;(set-args! reduction-scalar cl-data cl-partial-sums cl-partial-output)  ;setovanje promenjivih u kernelu        reduction-scalar
-                                                                                 ;cl-partial-sums=1024  i         
-        ;=> reduction-scalar                                                      ;cl-partial-output = cl_buffer objekat u kontekstu ctx velicine (4 * 2na20 / 256 = 2na14) i read-write ogranicenjima
+                                                                                      ;cl-partial-sums=1024  i         
+             ;=> reduction-scalar                                                      ;cl-partial-output = cl_buffer objekat u kontekstu ctx velicine (4 * 2na20 / 256 = 2na14) i read-write ogranicenjima
         
-        (enq-write! cqueue cl-data data) => cqueue                                 ;SETUJE VREDNOST GLOBALNE PROMENJIVE cl-data SA VREDNOSCU data
-        (enq-write! cqueue cl-brpolja brpolja) => cqueue        
+             (enq-write! cqueue cl-data (int-array (take-last 16500 data))) => cqueue                                 ;SETUJE VREDNOST GLOBALNE PROMENJIVE cl-data SA VREDNOSCU data
+             (enq-write! cqueue cl-brpolja brpolja) => cqueue        
         
-        (enq-nd! cqueue reduction-scalar                       ;asinhrono izvrsava kernel u uredjaju. cqueue, kernel koji se izvrsava
-                 (work-size [num-items]             ;[2na20]  sa ovim poljem vraca niz resenja (za svaki work-group posebno).
-                                                    ;ako ovo izbrisemo vraca resenje samo u prvom elementu niza tj. konacan zbir svih work-group 
-                            [256]                  ;[256] 
-                            )       
+             (enq-nd! cqueue reduction-scalar                       ;asinhrono izvrsava kernel u uredjaju. cqueue, kernel koji se izvrsava
+                      (work-size [num-items]             ;[2na20]  sa ovim poljem vraca niz resenja (za svaki work-group posebno).
+                                                         ;ako ovo izbrisemo vraca resenje samo u prvom elementu niza tj. konacan zbir svih work-group 
+                                 [256]                  ;[256] 
+                                 )       
                  nil profile-event)                            ;wait_event - da li da se ceka zavrsetak izvrsenja navedenih event-a tj proile-event1
-        (follow profile-event)
+             (follow profile-event)
         (enq-read! cqueue cl-output partial-output)
- 
-        ;(long (first partial-output)) => workgroup-size
-        ;(println "partial-output POJEDINACNA RESENJA: " (seq partial-output))
-
        
+        ;(long (first partial-output)) => workgroup-size
+              ;(println "partial-output POJEDINACNA RESENJA: " (seq partial-output))
+
+             
         (finish! cqueue)
         (println "Scalar reduction time:"
                  (-> (<!! notifications) :event profiling-info durations :end))
        ;(println "UKUPAN ZBIR MEDJUSUMA____" (apply + (float-array (seq partial-output))))
-       ;(println "UKUPAN ZBIR MEDJUSUMA____" (seq partial-output)) 
-       (println "    RESENJA____: " (disj (set (seq partial-output)) 0))        
+             ;(println "UKUPAN ZBIR MEDJUSUMA____" (seq partial-output)) 
+             (println "    RESENJA____: " (disj (set (seq partial-output)) 0))        
        ;(println "elemenata izlaza (" bytesize "=" num-items "*" Float/BYTES ") /" workgroup-size "=" (/ bytesize workgroup-size)) 
-
+             (println "---------------KRAJ -------------------")
         )
        
        (facts
-        (println "============ Najosnovniji algoritam ======================================")
-        ;; ============= Najosnovniji algoritam ====================================
-        (set-args! reduction-scalar2 cl-data cl-brpolja cl-partial-sums cl-partial-podaci cl-x cl-output) => reduction-scalar2
+        (println "============ Okretanje table ======================================")
+        ;; ============= Okretanje table ====================================
+             (set-args! reduction-scalar3 cl-output cl-brpolja cl-partial-sums cl-partial-podaci cl-x cl-y cl-output) => reduction-scalar3
+        ;(set-args! reduction-scalar cl-data cl-partial-sums cl-partial-output)  ;setovanje promenjivih u kernelu        reduction-scalar
+                                                                                      ;cl-partial-sums=1024  i         
+             ;=> reduction-scalar                                                      ;cl-partial-output = cl_buffer objekat u kontekstu ctx velicine (4 * 2na20 / 256 = 2na14) i read-write ogranicenjima
+        
+             (enq-write! cqueue cl-data data) => cqueue                                 ;SETUJE VREDNOST GLOBALNE PROMENJIVE cl-data SA VREDNOSCU data
+             (enq-write! cqueue cl-brpolja brpolja) => cqueue        
+        
+             (enq-nd! cqueue reduction-scalar3                       ;asinhrono izvrsava kernel u uredjaju. cqueue, kernel koji se izvrsava
+                      (work-size [num-items]             ;[2na20]  sa ovim poljem vraca niz resenja (za svaki work-group posebno).
+                                                         ;ako ovo izbrisemo vraca resenje samo u prvom elementu niza tj. konacan zbir svih work-group 
+                                 [1]                  ;[256] 
+                                 )       
+                 (events profile-event) profile-event3)                            ;wait_event - da li da se ceka zavrsetak izvrsenja navedenih event-a tj proile-event1
+             (follow profile-event3)
+        (enq-read! cqueue cl-output partial-output)
+       
+        ;(long (first partial-output)) => workgroup-size
+              ;(println "partial-output POJEDINACNA RESENJA: " (seq partial-output))
 
-        (enq-write! cqueue cl-data data) => cqueue                                 ;SETUJE VREDNOST GLOBALNE PROMENJIVE cl-data SA VREDNOSCU data
+             
+        (finish! cqueue)
+        (println "Scalar reduction time:"
+                 (-> (<!! notifications) :event profiling-info durations :end))
+       ;(println "UKUPAN ZBIR MEDJUSUMA____" (apply + (float-array (seq partial-output))))
+             ;(println "UKUPAN ZBIR MEDJUSUMA____" (seq partial-output)) 
+             (println "    RESENJA____: " (disj (set (seq partial-output)) 0))        
+       ;(println "elemenata izlaza (" bytesize "=" num-items "*" Float/BYTES ") /" workgroup-size "=" (/ bytesize workgroup-size)) 
+             (println "---------------KRAJ -------------------")
+        )       
+       
+       
+       
+       
+       
+       
+       
+       
+       (facts
+                 (println "============ Najosnovniji algoritam ======================================")
+        ;; ============= Najosnovniji algoritam ====================================
+       (set-args! reduction-scalar2 cl-data cl-brpolja cl-partial-sums cl-partial-podaci cl-x cl-output) => reduction-scalar2
+       
+        ;(enq-write! cqueue cl-data data) => cqueue                                 ;SETUJE VREDNOST GLOBALNE PROMENJIVE cl-data SA VREDNOSCU data
+        (enq-write! cqueue cl-data data) => cqueue
         (enq-write! cqueue cl-brpolja brpolja) => cqueue        
         
         (enq-nd! cqueue reduction-scalar2                       ;asinhrono izvrsava kernel u uredjaju. cqueue, kernel koji se izvrsava
-                 (work-size [num-items]             ;[2na20]  sa ovim poljem vraca niz resenja (za svaki work-group posebno).
-                                                    ;ako ovo izbrisemo vraca resenje samo u prvom elementu niza tj. konacan zbir svih work-group 
-                            [256]                  ;[256] 
-                            )       
-                 (events profile-event) profile-event2)                            ;wait_event - da li da se ceka zavrsetak izvrsenja navedenih event-a tj proile-event1
-        (follow profile-event2)
+               (work-size [num-items]             ;[2na20]  sa ovim poljem vraca niz resenja (za svaki work-group posebno).
+                                                  ;ako ovo izbrisemo vraca resenje samo u prvom elementu niza tj. konacan zbir svih work-group 
+                          [256]                  ;[256] 
+                          )       
+                 (events profile-event profile-event3) profile-event2)                            ;wait_event - da li da se ceka zavrsetak izvrsenja navedenih event-a tj proile-event1
+       (follow profile-event2)
         (enq-read! cqueue cl-output partial-output)
        
         (finish! cqueue)
@@ -145,6 +193,6 @@
         ;(println (seq data))
         )       
        
-       ))))
+              ))))
   
  ;(catch Exception e (println "Greska 11111111: " (.getMessage e))))
